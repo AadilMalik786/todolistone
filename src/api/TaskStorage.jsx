46a78114789b03b2f27a74d1a6a25942362fe82b
Aadil malik <adilmalik786687@gmail.com>
2023-08-13 15:51:47 +0530

    upload

--- src/api/TaskStorage.jsx
+++ src/api/TaskStorage.jsx
@@ -1,26 +1,51 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useRef, useState } from "react";
 const Tasks=createContext();
 const TaskStorage=(props)=>{
-    const [data,setData]=useState([])
+    // const myB`` =useRef();
+    const [data,setData]=useState([]);
+    const [editingTask,setEditingTask]=useState(null);
+    const myRef=useRef();
 const handleCreate=(e)=>{
     e.preventDefault();
     if(e.target[0].value.length!==0)
     setData([...data,{task:e.target[0].value,id:Math.random()*1000000}]);
+    e.target[0].value = "";
 }
 const handleDelete=(id)=>{
+    if (window.confirm("Do you want to delete this permanently?"))
 setData(data.filter((value)=>(value.id!==id)))
 }
-// const handleEdit(){
 
-// }
+const handleEdit = (tas) => {
+    setEditingTask(tas); // Set the task being edited
+    if (myRef.current) {
+      myRef.current.value = tas.task; // Set the textarea value to the task's value
+    }
+  };
+
+  
+  const handleSave = () => {
+    if (editingTask && myRef.current) {
+        const updatedData = data.map((item) =>
+        item.id === editingTask.id ? { ...item, task: myRef.current.value } : item
+        );
+        console.log(editingTask.id);
+      setData(updatedData);
+      setEditingTask(null); // Clear the editing state
+      myRef.current.value = ''; // Clear the textarea
+    }
+  };
+
+
+
 
     return(
         <>
-        <Tasks.Provider value={{data,handleCreate,handleDelete}}>
+        <Tasks.Provider value={{data,handleCreate,handleSave,handleDelete,handleEdit,myRef,editingTask}}>
             {props.children}
         </Tasks.Provider>
         </>
     )
 }
 export default TaskStorage;
-export {Tasks}
+export {Tasks};
--- src/component/TaskGen.jsx
+++ src/component/TaskGen.jsx
@@ -1,18 +1,20 @@
-import React, { useContext } from "react";
+import React, {useContext } from "react";
 // import TaskStorage from "../api/TaskStorage";
 import { Tasks } from "../api/TaskStorage";
 import { GrAdd } from "react-icons/gr";
 import {MdDeleteForever} from "react-icons/md";
 import {BiSolidEdit } from "react-icons/bi";
 const TaskGen=(props)=>{
-    const {data,handleCreate,handleDelete}= useContext(Tasks);
+    const {data,handleCreate,handleDelete,handleEdit,myRef,editingTask,handleSave}= useContext(Tasks);
+    // const myButtonRef =useRef();
     return(
         // <h1 className="text-[red]">hello adil</h1>
         <>
         <section className="bg-slate-400 h-[100vh] overflow-y-auto flex flex-col ">
             <form className="flex flex-col  items-center justify-center" onSubmit={handleCreate}>
-            <textarea className="mt-10 resize-none h-20 w-[50%]" cols="30" rows="10"></textarea>
+            <textarea className="mt-10 resize-none h-20 w-[50%]" cols="30" rows="10" ref={myRef}></textarea>
             <button className="mt-5 w-[5%] bg-[red] flex items-center justify-center h-10 rounded-[10px] bg-purple-500"><GrAdd/></button>
+             {editingTask ? <button className='py-2  flex justify-center bg-emerald-400 border-2 border-emerald-400  hover:bg-emerald-500 text-3xl w-[50%] text-white' onClick={()=>handleSave(myRef)}>SAVE</button> : ""}
             </form>
             <article className='flex flex-wrap justify-center gap-3  overflow-y-scroll '>
                 {
@@ -21,9 +23,8 @@ const TaskGen=(props)=>{
                         <h2>{value.task}</h2>
                         <div className=" flex  w-[80%] justify-between items-end  ">
                             <button className=" h-10 w-10 text-4xl text-indigo-500"onClick={()=>handleDelete(value.id)}><MdDeleteForever /></button>
-                            <button className=" h-10 w-10 text-4xl text-rose-400"><BiSolidEdit /></button>
+                            <button className=" h-10 w-10 text-4xl text-rose-400"  onClick={()=>handleEdit(value)}><BiSolidEdit /></button>
                         </div>
-        
                             </article>
                     }):<h2 className="text-[96px] capitalize"> no task available</h2>
                 }
@@ -31,5 +32,5 @@ const TaskGen=(props)=>{
         </section>
         </>
     )
-}
+};
 export default TaskGen;
