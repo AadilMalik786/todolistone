8b1918c1bc1b7819c1e11d48161b501b3dda5222
Aadil malik <adilmalik786687@gmail.com>
2023-08-23 10:48:57 +0530

    responsive

--- src/api/TaskStorage.jsx
+++ src/api/TaskStorage.jsx
@@ -1,51 +1,47 @@
 import React, { createContext, useRef, useState } from "react";
-const Tasks=createContext();
-const TaskStorage=(props)=>{
-    // const myB`` =useRef();
-    const [data,setData]=useState([]);
-    const [editingTask,setEditingTask]=useState(null);
-    const myRef=useRef();
-const handleCreate=(e)=>{
+const Tasks = createContext();
+const TaskStorage = (props) => {
+  // const myB`` =useRef();
+  const [data, setData] = useState([]);
+  const [editingTask, setEditingTask] = useState(null);
+  const myRef = useRef();
+  const handleCreate = (e) => {
     e.preventDefault();
-    if(e.target[0].value.length!==0)
-    setData([...data,{task:e.target[0].value,id:Math.random()*1000000}]);
+    if (e.target[0].value.length !== 0)
+      setData([...data, { task: e.target[0].value, id: Math.random() * 1000000 }]);
+    console.log([...data])
     e.target[0].value = "";
-}
-const handleDelete=(id)=>{
+  }
+  const handleDelete = (id) => {
     if (window.confirm("Do you want to delete this permanently?"))
-setData(data.filter((value)=>(value.id!==id)))
-}
+      setData(data.filter((value) => (value.id !== id)))
+  }
 
-const handleEdit = (tas) => {
+  const handleEdit = (tas) => {
     setEditingTask(tas); // Set the task being edited
     if (myRef.current) {
       myRef.current.value = tas.task; // Set the textarea value to the task's value
     }
   };
 
-  
+
   const handleSave = () => {
     if (editingTask && myRef.current) {
-        const updatedData = data.map((item) =>
-        item.id === editingTask.id ? { ...item, task: myRef.current.value } : item
-        );
-        console.log(editingTask.id);
+      const updatedData = data.map((item) =>
+      item.id === editingTask.id ? { ...item, task: myRef.current.value } : item
+      );
       setData(updatedData);
       setEditingTask(null); // Clear the editing state
       myRef.current.value = ''; // Clear the textarea
     }
   };
-
-
-
-
-    return(
-        <>
-        <Tasks.Provider value={{data,handleCreate,handleSave,handleDelete,handleEdit,myRef,editingTask}}>
-            {props.children}
-        </Tasks.Provider>
-        </>
-    )
+  return (
+    <>
+      <Tasks.Provider value={{ data, handleCreate, handleSave, handleDelete, handleEdit, myRef, editingTask }}>
+        {props.children}
+      </Tasks.Provider>
+    </>
+  )
 }
 export default TaskStorage;
-export {Tasks};
+export { Tasks };
--- src/component/TaskGen.jsx
+++ src/component/TaskGen.jsx
@@ -12,21 +12,21 @@ const TaskGen=(props)=>{
         <>
         <section className="bg-slate-400 h-[100vh] overflow-y-auto flex flex-col ">
             <form className="flex flex-col  items-center justify-center" onSubmit={handleCreate}>
-            <textarea className="mt-10 resize-none h-20 w-[50%]" cols="30" rows="10" ref={myRef}></textarea>
-            <button className="mt-5 w-[5%] bg-[red] flex items-center justify-center h-10 rounded-[10px] bg-purple-500"><GrAdd/></button>
-             {editingTask ? <button className='py-2  flex justify-center bg-emerald-400 border-2 border-emerald-400  hover:bg-emerald-500 text-3xl w-[50%] text-white' onClick={()=>handleSave(myRef)}>SAVE</button> : ""}
+            <textarea className="mt-10 resize-none h-20 w-[50%] textarea-class" cols="30" rows="10" ref={myRef}></textarea>
+            <button className="mt-5 w-[5%] bg-[red] flex items-center justify-center h-10 rounded-[10px] bg-purple-500 plusbutton-class"><GrAdd/></button>
+             {editingTask ? <button className='py-2  flex justify-center bg-emerald-400 border-2 border-emerald-400  hover:bg-emerald-500 text-3xl w-[50%] text-white mt-[20px] '  onClick={()=>handleSave()}>SAVE</button> : ""}
             </form>
-            <article className='flex flex-wrap justify-center gap-3  overflow-y-scroll '>
+            <article className='flex flex-wrap justify-center gap-3  overflow-y-scroll mt-[20px] flex-wrap'>
                 {
                     data.length !=0 ? data.map(value=>{
-                    return    <article className="flex flex-col justify-end gap-[100px] bg-rose-200 py-2 px-6 h-52 w-[30%]  rounded-xl" key={Math.random()*1000}>  
+                    return    <article className="flex flex-col justify-end gap-[100px] bg-rose-200 py-2 px-6 h-52 w-[30%]  rounded-xl box-class" key={Math.random()*1000}>  
                         <h2>{value.task}</h2>
                         <div className=" flex  w-[80%] justify-between items-end  ">
                             <button className=" h-10 w-10 text-4xl text-indigo-500"onClick={()=>handleDelete(value.id)}><MdDeleteForever /></button>
                             <button className=" h-10 w-10 text-4xl text-rose-400"  onClick={()=>handleEdit(value)}><BiSolidEdit /></button>
                         </div>
                             </article>
-                    }):<h2 className="text-[96px] capitalize"> no task available</h2>
+                    }):<h2 className="text-[96px] capitalize heading-class"> no task available</h2>
                 }
                 </article>
         </section>
